e84e49012c1e7bd4e92c337a2cd8adfa5c46dd85
caioportela <caiofabioportela@outlook.com>
2020-08-16 12:19:59 -0300

    feat: add signup action

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -1,13 +1,41 @@
+const SERVER = 'http://localhost:3000'
+
 const state = {
   token: localStorage.getItem('user-token') || '',
-  status: ''
+  user: {}
 }
 
 const getters = {
   isAuthenticated: (state) => Boolean(state.token)
 }
 
+const actions = {
+  signUp: async ({ commit }, { company, user }) => {
+    const res = await fetch(`${SERVER}/companies`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ company, user })
+    })
+
+    if(res.ok) {
+      const data = await res.json()
+
+      commit('setToken', data)
+    }
+  }
+}
+
+const mutations = {
+  setToken: (state, data) => {
+    localStorage.setItem('user-token', data.token)
+    state.token = data.token
+    state.user = data.user
+  }
+}
+
 export default {
+  actions,
   getters,
+  mutations,
   state
 }
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="self-center mt-8">
+  <div class="self-center mt-10">
     <div class="max-w-md w-full overflow-hidden text-center rounded-lg shadow-lg px-10 py-5 bg-gray-100">
       <h6 class="text-3xl text-teal-500 mb-4">Sign up</h6>
 
-      <form class="mb-4">
+      <form v-on:submit.prevent="signUp({ company, user })" class="mb-4">
         <div class="relative flex w-full items-center overflow-hidden mb-4">
           <span class="absolute inset-y-0 left-0 flex items-center text-gray-600 pl-2">
             <svg viewBox="0 0 20 20" fill="currentColor" class="home w-6 h-6">
@@ -11,7 +11,7 @@
             </svg>
           </span>
 
-          <input type="text" class="w-full transition duration-300 rounded-full py-2 pl-10 text-sm bg-gray-200 border-2 appearance-none rounded-tg focus:bg-white focus:outline-none focus:border-teal-300 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Company" autocomplete="off">
+          <input v-model="company.name" type="text" class="w-full transition duration-300 rounded-full py-2 pl-10 text-sm bg-gray-200 border-2 appearance-none rounded-tg focus:bg-white focus:outline-none focus:border-teal-300 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Company" autocomplete="off">
         </div>
 
         <div class="relative flex w-full items-center overflow-hidden mb-4">
@@ -21,7 +21,7 @@
             </svg>
           </span>
 
-          <input type="text" class="w-full transition duration-300 rounded-full py-2 pl-10 text-sm bg-gray-200 border-2 appearance-none rounded-tg focus:bg-white focus:outline-none focus:border-teal-300 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Name" autocomplete="off">
+          <input v-model="user.name" type="text" class="w-full transition duration-300 rounded-full py-2 pl-10 text-sm bg-gray-200 border-2 appearance-none rounded-tg focus:bg-white focus:outline-none focus:border-teal-300 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Name" autocomplete="off">
         </div>
 
         <div class="relative flex w-full items-center overflow-hidden mb-4">
@@ -31,7 +31,7 @@
             </svg>
           </span>
 
-          <input type="text" class="w-full transition duration-300 rounded-full py-2 pl-10 text-sm bg-gray-200 border-2 appearance-none rounded-tg focus:bg-white focus:outline-none focus:border-teal-300 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Username" autocomplete="off">
+          <input v-model="user.username" type="text" class="w-full transition duration-300 rounded-full py-2 pl-10 text-sm bg-gray-200 border-2 appearance-none rounded-tg focus:bg-white focus:outline-none focus:border-teal-300 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Username" autocomplete="off">
         </div>
 
         <div class="relative flex w-full items-center overflow-hidden mb-4">
@@ -41,23 +41,30 @@
             </svg>
           </span>
 
-          <input type="password" class="w-full transition duration-300 rounded-full py-2 pl-10 text-sm bg-gray-200 border-2 appearance-none rounded-tg focus:bg-white focus:outline-none focus:border-teal-300 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Password" autocomplete="off">
+          <input v-model="user.password" type="password" class="w-full transition duration-300 rounded-full py-2 pl-10 text-sm bg-gray-200 border-2 appearance-none rounded-tg focus:bg-white focus:outline-none focus:border-teal-300 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Password" autocomplete="off">
         </div>
 
-        <div class="relative flex w-full items-center overflow-hidden mb-4">
-          <span class="absolute inset-y-0 left-0 flex items-center text-gray-600 pl-2">
-            <svg viewBox="0 0 20 20" fill="currentColor" class="key w-6 h-6">
-              <path fill-rule="evenodd" d="M18 8a6 6 0 01-7.743 5.743L10 14l-1 1-1 1H6v2H2v-4l4.257-4.257A6 6 0 1118 8zm-6-4a1 1 0 100 2 2 2 0 012 2 1 1 0 102 0 4 4 0 00-4-4z" clip-rule="evenodd"></path>
-            </svg>
-          </span>
-
-          <input type="password" class="w-full transition duration-300 rounded-full py-2 pl-10 text-sm bg-gray-200 border-2 appearance-none rounded-tg focus:bg-white focus:outline-none focus:border-teal-300 focus:text-gray-900 focus:shadow-outline-blue" placeholder="Confirm Password" autocomplete="off">
-        </div>
-
-        <button type="button" class="bg-teal-400 hover:bg-teal-600 transition duration-300 focus:outline-none focus:shadow-none text-lg rounded-full px-10 py-1 text-white">Register</button>
+        <button type="submit" class="bg-teal-400 hover:bg-teal-600 transition duration-300 focus:outline-none focus:shadow-none text-lg rounded-full px-10 py-1 text-white">Register</button>
       </form>
 
       <router-link to="/signin" class="text-teal-500 hover:text-teal-600">Sign in</router-link>
     </div>
   </div>
 </template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Signup',
+  methods: {
+    ...mapActions(['signUp'])
+  },
+  data: function() {
+    return {
+      company: {},
+      user: {}
+    }
+  }
+}
+</script>
