3ed332a8804e59021b535160434e14329f9da8cb
caioportela <caiofabioportela@outlook.com>
2020-08-16 20:54:16 -0300

    feat: setup sweet-alert2

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueSweetalert2 from 'vue-sweetalert2'
+
 import '@/assets/styles/tailwind.css'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
+Vue.use(VueSweetalert2)
 
 Vue.config.productionTip = false
 
