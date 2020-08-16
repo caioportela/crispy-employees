3608bf6682a89d2751c75d8eec6dad12bb9d7ee9
caioportela <caiofabioportela@outlook.com>
2020-08-16 15:17:08 -0300

    feat: add company name

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="min-h-screen h-screen flex flex-col items-center text-gray-800 bg-gray-300 py-8 px-12">
+    <h6 class="font-serif text-teal-600 text-4xl">
+      <span v-if="showCompany">{{company.name}}</span>
+      <span v-else>Crispy</span>
+    </h6>
+
     <router-view/>
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'App',
+  computed: {
+    ...mapGetters(['company']),
+
+    showCompany: function() {
+      return Boolean(this.company)
+    }
+  }
+}
+</script>
